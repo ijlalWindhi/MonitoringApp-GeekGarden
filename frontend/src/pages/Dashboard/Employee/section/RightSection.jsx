2dd67b5b3f9d8a157dafd1c90def123cb84bd4a2
ijlalWindhi <ijlalwindhi15@gmail.com>
2022-06-10 15:03:40 +0700

    add timer and salary

---
+++ frontend/src/pages/Dashboard/Employee/section/RightSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Timer from '../../../../components/fragment/Timer/Timer'
+
+export default function RightSection() {
+  return (
+    <div>
+        <Timer/>
+        <h1></h1>
+    </div>
+  )
+}
