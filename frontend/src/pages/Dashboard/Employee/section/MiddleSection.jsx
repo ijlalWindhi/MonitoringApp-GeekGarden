7e641797e355637a3c7a69aa1b5909d0cedfce07
ijlalWindhi <ijlalwindhi15@gmail.com>
2022-06-18 22:54:38 +0700

    add card all project

--- frontend/src/pages/Dashboard/Employee/section/MiddleSection.jsx
+++ frontend/src/pages/Dashboard/Employee/section/MiddleSection.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import CardAllProject from '../../../../components/fragment/Card All Project/CardAllProject'
 
 export default function MiddleSection() {
   return (
-    <div>MiddleSection</div>
+    <div>
+      <CardAllProject/>
+    </div>
   )
 }
